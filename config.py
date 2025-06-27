4d946c6a7faf7542b20e05b9c30528d0dad5ec0a
huang-33 <2300012415@stu.pku.edu.cn>
2025-06-27 18:21:56 +0800

    修改config

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ PORT = '3306'
 DATABASE = 'QAPlatform'
 USERNAME = 'root'
 PASSWORD = 'root'
-# DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
-DB_URI = "mysql+pymysql://root:FQAaMlJgHzmLkVhrjFwOAZNXmIrQXGbR@mainline.proxy.rlwy.net:16309/railway"
-# SQLALCHEMY_DATABASE_URI = DB_URI
-SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+# DB_URI = "mysql+pymysql://root:FQAaMlJgHzmLkVhrjFwOAZNXmIrQXGbR@mainline.proxy.rlwy.net:16309/railway"
+SQLALCHEMY_DATABASE_URI = DB_URI
+# SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
 
 # 邮箱配置
 MAIL_SERVER = "smtp.qq.com"
